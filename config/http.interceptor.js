63f1e3772abfcab685b902f8582253ba27cf0830
yinmang1215@163.com <yinmang1215@163.com>
2022-05-27 10:23:32 +0800

    去除打印信息

--- config/http.interceptor.js
+++ config/http.interceptor.js
@@ -16,7 +16,6 @@ module.exports = (vm) => {
 	    // 初始化请求拦截器时，会执行此方法，此时data为undefined，赋予默认{}
 	    config.data = config.data || {}
       // 根据custom参数中配置的是否需要token，添加对应的请求头
-      console.log(config)
       if(config?.custom?.auth) {
         // 可以在此通过vm引用vuex中的变量，具体值在vm.$store.state中
         config.header.Authorization = 'Bearer ' + storage.get(ACCESS_TOKEN)
