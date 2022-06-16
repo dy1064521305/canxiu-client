76dd11fb664789c383044de5c15aa873455c1b95
yinmang1215@163.com <yinmang1215@163.com>
2022-06-16 15:14:07 +0800

    公告详情适配小程序

--- pages/work/notice/detail.vue
+++ pages/work/notice/detail.vue
@@ -1,5 +1,6 @@
 <template>
   <view class="mobile-item-container">
+    <Navbar title="公告详情" bgColor="#fff" :h5Show="false"></Navbar>
     <view style="font-size: 40rpx; font-weight: bold;">{{notice.noticeTitle}}</view>
     <view style="display: flex; font-size: 12px; color: #909399;">
       <u-icon name="clock" size="12"></u-icon>
@@ -15,16 +16,20 @@
 
 <script>
 import * as NoticeApi from '@/api/work/notice'
+import Navbar from '@/components/navbar/Navbar'
 
 export default {
+  components: {
+    Navbar,
+  },
   data () {
     return {
       noticeId: undefined,
       notice: {}
     }
   },
-  onShow () {
-    this.noticeId = this.$route.query.id
+  onLoad (params) {
+    this.noticeId = params.id
     this.loadData()
   },
   methods: {
