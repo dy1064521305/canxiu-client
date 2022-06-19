6029e2d8793d8fb41e94636758b1fda7d1d6ed0a
yinmang1215@163.com <yinmang1215@163.com>
2022-06-19 21:41:16 +0800

    配置h5端口,引发devServer冲突问题处理

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,12 @@ const { environment } = require('./config/environment.js')
 
 module.exports = {
   devServer: {
+    port: 9001,
     proxy: {
       '/': {
         target: environment.baseURL,
-		ws: true,
-		changeOrigin: true,
+        ws: true,
+        changeOrigin: true,
         pathRewrite: {
           '^/': ''
         }
