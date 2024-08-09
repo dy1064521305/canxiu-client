76eb023960555cc0e70b21fead27dbff5de49b19
Trigg141 <long11490@126.com>
2024-08-09 17:56:52 +0800

    启动页隐藏

--- config/environment.js
+++ config/environment.js
@@ -8,8 +8,8 @@ const environment = {
 		//baseURL: 'http://192.168.124.12:8080',
 		// baseURL: 'http://192.168.100.123:8080',
 		 // baseURL: 'http://192.168.100.122:8080',
-	     baseURL: 'http://121.40.253.155:8080',
-		// baseURL: 'https://www.canxiukeji.com/prod-api',
+	     // baseURL: 'http://121.40.253.155:8080',
+		baseURL: 'https://www.canxiukeji.com/prod-api',
 		// baseURL: 'https://www-test.canxiukeji.com/prod-api',
 
 	},
@@ -17,8 +17,8 @@ const environment = {
 	production: {
 		// baseURL: 'http://121.41.107.68:8080' ,// 发布时需要修改为后端实际地址
 		// baseURL: 'http://121.40.253.155:8080' ,// 发布时需要修改为后端实际地址
-		// baseURL: 'https://www.canxiukeji.com/prod-api',
-		baseURL: 'https://www-test.canxiukeji.com/prod-api',
+		baseURL: 'https://www.canxiukeji.com/prod-api',
+		// baseURL: 'https://www-test.canxiukeji.com/prod-api',
 	}
 }
 
